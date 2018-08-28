d1ca4b71ad711aac70e347d8fdcc5f27f90aaa5f
Ewen Steele <ewen.steele@btinternet.com>
2018-08-28 11:24:08 +0100

    Updated Timeline

--- AboutMe/AboutMe.js
+++ AboutMe/AboutMe.js
@@ -1,12 +1,12 @@
 function isElementInViewport(el) {
     var rect = el.getBoundingClientRect();
-return(
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-)
-}
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
 var items = document.querySelectorAll(".timeline li");
  
 // code for the isElementInViewport function
